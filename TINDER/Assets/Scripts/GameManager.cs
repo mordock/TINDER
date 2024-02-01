f991694dbaca6cbe248f17780accad1472109150
micahcalis <micahcalis2015@gmail.com>
2024-02-01 14:19:38 +0100

    Timeline sytem + videoplayer

--- TINDER/Assets/Scripts/GameManager.cs
+++ TINDER/Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
@@ -23,30 +25,38 @@ public class GameManager : MonoBehaviour
     private float distortIntensity;
     [SerializeField]
     private float chromaticIntensity;
-    private float distort;
-    private float chromatic;
-
+    [SerializeField]
+    private GameObject dirLight;
+    private float currentAngle = -100;
+    [SerializeField]
+    private float targetAngle;
+    [SerializeField]
+    private float lightLerpSpeed;
+    private float timer;
+    [SerializeField]
+    private float[] timeStamps;
+    private float fadeIntensity = 1;
+    private float fadeTarget;
+    private float saturationIntensity;
+    private float saturationTarget;
+    private float glitchFrequency;
+    private bool canStartGlitchLoop = true;
 
     void Start()
     {
         boostMeter = maxBoost;
         pcb = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerBoat>();
+        postMat.SetFloat("_FadeIntensity", fadeIntensity);
+        fadeTarget = fadeIntensity;
+        saturationTarget = 1.5f;
     }
 
     void Update()
     {
-        if (boostMeter < maxBoost && !pcb.isBoosting)
-        {
-            boostMeter += chargeSpeed * Time.deltaTime;
-        }
-        if(boostMeter > 0 && pcb.isBoosting)
-        {
-            boostMeter -= chargeSpeed * 2 * Time.deltaTime;
-        }
-        if(boostMeter > 0.1) { pcb.canBoost = true; }
-        else { pcb.canBoost = false; }
-
-        PostEffect();
+        TimeLine();
+        HandleBoost();
+        LerpFade();
+        LerpSaturation();
     }
     public void StartHitstunRoutine()
     {
@@ -68,19 +78,86 @@ public class GameManager : MonoBehaviour
         isHit = false;
     }
 
-    private void PostEffect()
+    private void HandleBoost()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (boostMeter < maxBoost && !pcb.isBoosting)
         {
-            distort = distortIntensity;
-            chromatic = chromaticIntensity; 
+            boostMeter += chargeSpeed * Time.deltaTime;
         }
-        if (Input.GetKeyUp(KeyCode.E))
+        if (boostMeter > 0 && pcb.isBoosting)
         {
-            distort = 0;
-            chromatic = 0;
+            boostMeter -= chargeSpeed * 2 * Time.deltaTime;
         }
-        postMat.SetFloat("_DistortIntensity", distort);
-        postMat.SetFloat("_ChromaticIntensity", chromatic);
+        if (boostMeter > 0.1) { pcb.canBoost = true; }
+        else { pcb.canBoost = false; }
+    }
+    private void TimeLine()
+    {
+        timer = Time.time;
+        if (timer > timeStamps[0])
+        {
+            fadeTarget = 0.97f;
+        }
+        if (timer > timeStamps[1])
+        {
+            fadeTarget = -0.5f;
+            LerpDirLight();
+        }
+        if (timer > timeStamps[2])
+        {
+            if (canStartGlitchLoop)
+            {
+                glitchFrequency = 0.5f;
+                StartCoroutine(Glitch());
+                canStartGlitchLoop = false;
+            }
+        }
+        if (timer > timeStamps[3])
+        {
+            glitchFrequency = 0.5f;
+        }
+    }
+
+    private IEnumerator Glitch()
+    {
+        yield return new WaitForSeconds(glitchFrequency);
+        int i = Random.Range(0, 2);
+        if (i == 0)
+        {
+            postMat.SetFloat("_DistortIntensity", distortIntensity);
+            postMat.SetFloat("_ChromaticIntensity", chromaticIntensity);
+        }
+        yield return new WaitForSeconds(0.2f);
+        if (i == 0)
+        {
+            postMat.SetFloat("_DistortIntensity", 0);
+            postMat.SetFloat("_ChromaticIntensity", 0);
+        }
+        StartCoroutine(Glitch());
+    }
+    private void LerpDirLight()
+    {
+        if(currentAngle < 60)
+        currentAngle = Mathf.LerpAngle(currentAngle, targetAngle, Time.deltaTime * lightLerpSpeed);
+        else
+        {
+            currentAngle = 60;
+        }
+        dirLight.transform.eulerAngles = new Vector3(currentAngle, 0, 0);
+    }
+
+    private void LerpFade()
+    {
+
+        fadeIntensity = Mathf.Lerp(fadeIntensity, fadeTarget, Time.deltaTime * lightLerpSpeed);
+        fadeIntensity = Mathf.Clamp(fadeIntensity, 0, 1);
+        postMat.SetFloat("_FadeIntensity", fadeIntensity);
+    }
+
+    private void LerpSaturation()
+    {
+        saturationIntensity = Mathf.Lerp(saturationIntensity, saturationTarget, Time.deltaTime * lightLerpSpeed * 0.2f);
+        saturationIntensity = Mathf.Clamp(saturationIntensity, 0, 1);
+        postMat.SetFloat("_SaturationIntensity", saturationIntensity);
     }
 }
